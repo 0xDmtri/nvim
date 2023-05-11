76586754b2c217bb692a50610e770acda7aa1d0a
0xDmtri <0xDmtri@protonmail.com>
2023-05-11 16:34:10 +0100

    Switch binds

--- lua/0xDmtri/configs/fterm.lua
+++ lua/0xDmtri/configs/fterm.lua
@@ -1,3 +1,3 @@
 -- [[ Configure FTerm ]]
-vim.keymap.set('n', '\\', '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set('t', '\\', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('n', '<C-\\>', '<CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set('t', '<C-\\>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
--- lua/0xDmtri/configs/neotree.lua
+++ lua/0xDmtri/configs/neotree.lua
@@ -1,4 +1,4 @@
 -- [[ Configure Neotree]]
-vim.keymap.set("n", "<C-\\>", ":Neotree toggle reveal_force_cwd <CR>", { silent = true })
+vim.keymap.set("n", "\\", ":Neotree toggle reveal_force_cwd <CR>", { silent = true })
 vim.keymap.set("n", "<leader>\\b", ":Neotree source=buffers toggle<CR>", { desc = "Neotree Buffer" })
 vim.keymap.set("n", "<leader>\\g", ":Neotree source=git_status toggle<CR>", { desc = "Neotree Git Status" })
