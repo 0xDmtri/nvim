da2f2cd872fa3767d422527dc69040825e6ee2da
0xDmtri <0xDmtri@protonmail.com>
2023-06-16 17:22:37 +0100

    add JS syntax highlight ((((

--- lua/0xDmtri/configs/treesitter.lua
+++ lua/0xDmtri/configs/treesitter.lua
@@ -2,8 +2,8 @@
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'solidity',
-        'markdown', 'markdown_inline', 'toml' },
+    ensure_installed = { 'c', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'javascript', 'vimdoc', 'vim',
+        'solidity', 'markdown', 'markdown_inline', 'toml' },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
