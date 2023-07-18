cdb97f71f815a09c476dd11b036f0676b89e75b8
0xDmtri <0xDmtri@protonmail.com>
2023-07-18 20:15:01 +0100

    bump neo-tree to v3.x

--- lua/0xDmtri/init.lua
+++ lua/0xDmtri/init.lua
@@ -245,7 +245,7 @@ require('lazy').setup({
   {
     -- Better file tree
     "nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
+    branch = "v3.x",
     dependencies = {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons",
