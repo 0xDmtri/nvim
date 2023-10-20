65219603cbc3857fa79f4b3cbaeb71894a65d862
0xDmtri <0xDmtri@protonmail.com>
2023-10-20 18:17:09 +0100

    fix gitsigns

--- lua/0xDmtri/init.lua
+++ lua/0xDmtri/init.lua
@@ -133,6 +133,7 @@ require('lazy').setup({
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
+    event = "BufReadPre",
     opts = {
       -- See `:help gitsigns.txt`
       signs = {
